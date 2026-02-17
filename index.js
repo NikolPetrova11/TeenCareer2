458fd72829f82dfe33bdfbd2a7f83b679c0c9c18
NikolPetrova11 <nikolpetrova29@itpg-varna.bg>
2026-02-17 21:51:48 +0200

    Adi e dosadna

--- index.js
+++ index.js
@@ -29,4 +29,68 @@ function updateHeaderHeight() {
   window.addEventListener('load', updateHeaderHeight);
   window.addEventListener('resize', updateHeaderHeight);
   
+  const menuToggle = document.getElementById("menuToggle");
+  const navMenu = document.getElementById("navMenu"); 
+
+  menuToggle.addEventListener("click", () => {
+    
+    if (navMenu) {
+        navMenu.classList.toggle("active");
+    }
+  });
+
   
+  const profileBtn = document.querySelector(".profile-btn");
+  const overlay = document.querySelector(".overlay");
+  const popup = document.querySelector(".popup");
+  const closeBtn = document.querySelector(".popup .close-btn");
+
+  profileBtn.addEventListener("click", function() {
+    overlay.classList.add("active");
+    popup.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", function() {
+    overlay.classList.remove("active");
+    popup.classList.remove("active");
+  });
+
+  overlay.addEventListener("click", function(e) {
+    if (e.target.classList.contains("overlay")) {
+        overlay.classList.remove("active");
+        popup.classList.remove("active");
+    }
+  });
+
+
+  const tabButtons = document.querySelectorAll(".tab-btn");
+  const loginForm = document.querySelector(".login-form-container");
+  const registerForm = document.querySelector(".register-form-container");
+
+  tabButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        tabButtons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const targetForm = button.getAttribute("data-form");
+
+        if (targetForm === "login") {
+            loginForm.classList.add("active");
+            registerForm.classList.remove("active");
+        } else if (targetForm === "register") {
+            loginForm.classList.remove("active");
+            registerForm.classList.add("active");
+        }
+    });
+  });
+
+  
+function klik() {
+  window.location.href = "searching.html";
+}
+
+function toggleChat() {
+  const box = document.getElementById("chatBox");
+  box.style.display = (box.style.display === "flex") ? "none" : "flex";
+}
+
